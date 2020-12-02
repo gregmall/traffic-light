53d39283c9ac76ac4758adb29bb41fa73e93daca
Greg Mall <gregmall157@gmail.com>
2020-12-02 15:03:13 -0800

    working

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './App.css';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  const [color, setColor] = useState('red');
+  const [light, setLight] = useState('RED');
+   const handleClick = ({ target }) => {
+     if(target.name === 'red'){
+       setColor('red');
+       setLight('RED');
+     }
+     if(target.name === 'yellow'){
+      setColor('yellow');
+      setLight('YELLOW');
+     }
+     if(target.name ==='green'){
+      setColor('green');
+      setLight('GREEN');
+       console.log(color);
+     }
+
+
+   }
+  return (
+    <>
+    <h1 className={styles[color]}>{light}</h1>
+    <button name ="red" onClick={handleClick}>red</button>
+    <button name="yellow" onClick={handleClick}>yellow</button>
+    <button name="green" onClick={handleClick}>green</button>
+    </>
+  );
 }
