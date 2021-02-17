09bcf86505dac781ee52026b75674b70dd0c683b
Greg Mall <gregmall157@gmail.com>
2021-02-16 20:16:10 -0800

    edits

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -4,7 +4,7 @@ import styles from './App.css';
 export default function App() {
   const [color, setColor] = useState('red');
   const [light, setLight] = useState('RED');
-   const handleClick = ({ target }) => {
+  const handleClick = ({ target }) => {
      if(target.name === 'red'){
        setColor('red');
        setLight('RED');
@@ -18,22 +18,20 @@ export default function App() {
       setLight('GREEN');
       
      }
-
-
-   }
+  }
   return (
     <>
-    <div className = {styles.main}>
-    <div className = {styles[color]}>
-      <div className ={styles.one}></div>
-    <div className={styles.two}></div>
-    <div className={styles.three}></div>
-    </div>
-    </div>
-    
-    <button name ="red" onClick={handleClick}>red</button>
-    <button name="yellow" onClick={handleClick}>yellow</button>
-    <button name="green" onClick={handleClick}>green</button>
+      <div className = {styles.main}>
+        <div className = {styles[color]}>
+          <div className ={styles.one}></div>
+          <div className={styles.two}></div>
+          <div className={styles.three}></div>
+        </div>
+      </div>
+      
+      <button name ="red" onClick={handleClick}>red</button>
+      <button name="yellow" onClick={handleClick}>yellow</button>
+      <button name="green" onClick={handleClick}>green</button>
     </>
   );
 }
